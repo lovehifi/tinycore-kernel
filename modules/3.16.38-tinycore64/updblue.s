a0742c5b1fd19b1f58cc0e8c96fb0a6fdc866f19
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-23 19:12:30 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -1,23 +1,23 @@
 #!/bin/sh
-# URD=https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64
+TM=/tmp
+URD=https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64
 # mkdir /tmp/peppy_screensaver
 # wget -O /tmp/peppy_screensaver.zip https://github.com/2aCD-creator/volumio-plugins/raw/gh-pages/plugins/volumio/armhf/miscellanea/peppy_screensaver/peppy_screensaver.zip
 # miniunzip /tmp/peppy_screensaver.zip -d /tmp/peppy_screensaver/
 # cd /tmp/peppy_screensaver/
 # volumio plugin install
-#
-#wget -O /tmp/tmpl.tar.gz $URD/tmpl.tar.gz
-#tar -xf /tmp/tmpl.tar.gz --overwrite -C /
-#wget -O /tmp/peppy_screensaver.tar.gz $URD/peppy_screensaver.tar.gz
-#wget -O /tmp/peppy_config.tar.gz $URD/peppy_config.tar.gz
-#wget -O /tmp/configuration.tar.gz $URD/configuration.tar.gz
-#wget -O /tmp/peppyinstall.tar.gz $URD/peppyinstall.tar.gz
-#tar -xf /tmp/peppy_screensaver.tar.gz --overwrite -C /
-#tar -xf /tmp/configuration.tar.gz --overwrite -C /
-#tar -xf /tmp/peppyinstall.tar.gz --overwrite -C /
-#tar -xf /tmp/peppy_config.tar.gz --overwrite -C /
-#/data/plugins/miscellanea/peppy_screensaver/install.sh
-# sleep 5
+wget -O $TM/tmpl.tar.gz $URD/tmpl.tar.gz
+tar -xf $TM/tmpl.tar.gz --overwrite -C /
+wget -O $TM/peppy_screensaver.tar.gz $URD/peppy_screensaver.tar.gz
+wget -O $TM/peppy_config.tar.gz $URD/peppy_config.tar.gz
+wget -O $TM/configuration.tar.gz $URD/configuration.tar.gz
+wget -O $TM/peppyinstall.tar.gz $URD/peppyinstall.tar.gz
+tar -xf $TM/peppy_screensaver.tar.gz --overwrite -C /
+tar -xf $TM/configuration.tar.gz --overwrite -C /
+tar -xf $TM/peppyinstall.tar.gz --overwrite -C /
+tar -xf $TM/peppy_config.tar.gz --overwrite -C /
+/data/plugins/miscellanea/peppy_screensaver/install.sh
+sleep 5
 #reboot
 #touch /tmp/update
 exit
