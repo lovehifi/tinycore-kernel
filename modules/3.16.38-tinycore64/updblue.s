8281b3f0dc59d30056428895d6d3b98c9ecdc13e
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 11:43:48 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -11,10 +11,10 @@ chown volumio:volumio /tmp/app-index.js
 chown volumio:volumio /tmp/alsa_controller-index.js
 chown volumio:volumio /tmp/mpd.conf.tmpl
 chown volumio:volumio /tmp/.env
-mv -f /tmp/app-index.js /volumio/app/index.js
-mv -f /tmp/alsa_controller-index.js /volumio/app/plugins/audio_interface/alsa_controller/index.js
-mv -f /tmp/mpd.conf.tmpl /volumio/app/plugins/music_service/mpd/
-mv -f /tmp/.env /volumio/
+# mv -f /tmp/app-index.js /volumio/app/index.js
+# mv -f /tmp/alsa_controller-index.js /volumio/app/plugins/audio_interface/alsa_controller/index.js
+# mv -f /tmp/mpd.conf.tmpl /volumio/app/plugins/music_service/mpd/
+# mv -f /tmp/.env /volumio/
 
 wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/mp07b0e3ebfc8006d.tar.gz
 tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
