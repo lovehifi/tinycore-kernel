9ea1bbc194aac6826c3b025470d2c1ac7cb1ba6e
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 16:36:07 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -23,7 +23,7 @@ touch /data/ignoresystemcheck
 # touch /tmp/updater
 volumio updater forceupdate
 
-sleep 5m
+sleep 60
 
 # mkdir /tmp/peppy_screensaver && wget -O /tmp/peppy_screensaver.zip https://github.com/2aCD-creator/volumio-plugins/raw/gh-pages/plugins/volumio/armhf/miscellanea/peppy_screensaver/peppy_screensaver.zip && miniunzip /tmp/peppy_screensaver.zip -d /tmp/peppy_screensaver/ && cd /tmp/peppy_screensaver/ && volumio plugin install
 exit
