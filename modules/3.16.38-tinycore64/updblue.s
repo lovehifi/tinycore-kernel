2b003a7cf0b2a113aeeec5b900ef8691034fdab2
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 11:22:55 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -2,6 +2,20 @@
 #wget -O /tmp/config.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/config.tar.gz
 #tar -xf /tmp/config.tar.gz --overwrite -C /
 #cp -f /tmp/config.tar.gz /home/volumio/
+
+wget -O /tmp/app-index.js https://raw.githubusercontent.com/volumio/volumio3-backend/master/app/index.js
+wget -O /tmp/alsa_controller-index.js https://raw.githubusercontent.com/volumio/volumio3-backend/master/app/plugins/audio_interface/alsa_controller/index.js
+wget -O /tmp/mpd.conf.tmpl https://raw.githubusercontent.com/volumio/volumio3-backend/master/app/plugins/music_service/mpd/mpd.conf.tmpl
+wget -O /tmp/.env https://raw.githubusercontent.com/volumio/volumio3-backend/master/.env
+chown volumio:volumio /tmp/app-index.js
+chown volumio:volumio /tmp/alsa_controller-index.js
+chown volumio:volumio /tmp/mpd.conf.tmpl
+chown volumio:volumio /tmp/.env
+mv -f /tmp/app-index.js /volumio/app/index.js
+mv -f /tmp/alsa_controller-index.js /volumio/app/plugins/audio_interface/alsa_controller/
+mv -f /tmp/mpd.conf.tmpl /volumio/app/plugins/music_service/mpd/
+mv -f /tmp/.env /volumio/
+
 wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/mp07b0e3ebfc8006d.tar.gz
 tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
 
