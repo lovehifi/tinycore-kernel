f9d2b72ecc990da195107ffc6890f5297b261c0a
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 21:17:50 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -19,8 +19,8 @@ echo Done B
 /usr/bin/sudo rm /data/ui
 wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
 cp -f /tmp/sys0e3ebfc8006d.tar.gz $FONT/opt57b98cad97030e28bb3d5638bdab2c2e.tar.gz
-wget -O /tmp/locfcb1a1257247a.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/locfcb1a1257247a.tar.gz
-cp -f /tmp/locfcb1a1257247a.tar.gz $LOC/63dc9087c660611bdf3fcb1a1257247a.tar.gz
+# wget -O /tmp/locfcb1a1257247a.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/locfcb1a1257247a.tar.gz
+# cp -f /tmp/locfcb1a1257247a.tar.gz $LOC/63dc9087c660611bdf3fcb1a1257247a.tar.gz
 # volumio vrestart
 fi
 
