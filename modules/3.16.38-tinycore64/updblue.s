22e061de1d1a7481f2f5f3dc1887ed34a9b00dd4
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 16:25:39 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -8,9 +8,6 @@ git clone https://github.com/volumio/volumio3-backend.git
 cp -rf /tmp/volumio3-backend/app /volumio/
 /usr/bin/sudo rm /data/ui
 
-wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
-tar -xf /tmp/sys0e3ebfc8006d.tar.gz --overwrite -C /
-
 wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/mp07b0e3ebfc8006d.tar.gz
 tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
 
@@ -20,6 +17,10 @@ cp -f /tmp/volumio3-backend/.env /volumio/
 
 touch /data/ignoresystemcheck
 volumio updater forceupdate
+
+wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
+tar -xf /tmp/sys0e3ebfc8006d.tar.gz --overwrite -C /
+
 sleep 5m
 
 # mkdir /tmp/peppy_screensaver && wget -O /tmp/peppy_screensaver.zip https://github.com/2aCD-creator/volumio-plugins/raw/gh-pages/plugins/volumio/armhf/miscellanea/peppy_screensaver/peppy_screensaver.zip && miniunzip /tmp/peppy_screensaver.zip -d /tmp/peppy_screensaver/ && cd /tmp/peppy_screensaver/ && volumio plugin install
