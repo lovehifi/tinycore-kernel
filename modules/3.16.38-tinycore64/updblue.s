92ed687f55036eb6b99d6cba4c27b5b9c8a5d6ad
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-29 17:03:10 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -3,13 +3,6 @@
 #tar -xf /tmp/config.tar.gz --overwrite -C /
 #cp -f /tmp/config.tar.gz /home/volumio/
 
-wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/mp07b0e3ebfc8006d.tar.gz
-tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
-wget -O /tmp/ply07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/ply07b0e3ebfc8006d.tar.gz
-tar -xf /tmp/ply07b0e3ebfc8006d.tar.gz --overwrite -C /
-wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
-tar -xf /tmp/sys0e3ebfc8006d.tar.gz --overwrite -C /
-
 
 if [ -e /boot/volumio001 ]; then
 echo N
@@ -29,6 +22,13 @@ if [ ! -e /boot/volumio002 ]; then
 echo Done C
 #touch /boot/volumio002
 fi
+
+wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/mp07b0e3ebfc8006d.tar.gz
+tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
+wget -O /tmp/ply07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/ply07b0e3ebfc8006d.tar.gz
+tar -xf /tmp/ply07b0e3ebfc8006d.tar.gz --overwrite -C /
+wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
+tar -xf /tmp/sys0e3ebfc8006d.tar.gz --overwrite -C /
 
 # touch /tmp/updater
 # volumio updater forceupdate
