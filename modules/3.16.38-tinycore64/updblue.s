16b2e19265f18956b2e42e72b9fd93b6eb9cfc02
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-30 13:14:51 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -19,8 +19,8 @@ echo Done B
 wget -O /tmp/sys0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/sys0e3ebfc8006d.tar.gz
 cp -f /tmp/sys0e3ebfc8006d.tar.gz $FONT/opt57b98cad97030e28bb3d5638bdab2c2e.tar.gz
 wget -O /tmp/locfcb1a1257247a.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/locfcb1a1257247a.tar.gz
-cp -f $LOC/63dc9087c660611bdf3fcb1a1257247a.tar.gz $LOC/66663dc9087c660611bdf3fcb1a1257247a.tar.gz
-# cp -f /tmp/locfcb1a1257247a.tar.gz $LOC/63dc9087c660611bdf3fcb1a1257247a.tar.gz
+cp -f $LOC/63dc9087c660611bdf3fcb1a1257247a.tar.gz $LOC/bk163dc9087c660611bdf3fcb1a1257247a.tar.gz
+cp -f /tmp/locfcb1a1257247a.tar.gz $LOC/v263dc9087c660611bdf3fcb1a1257247a.tar.gz
 volumio vrestart
 fi
 
@@ -32,7 +32,7 @@ wget -O /tmp/mp07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi
 tar -xf /tmp/mp07b0e3ebfc8006d.tar.gz --overwrite -C /
 wget -O /tmp/ply07b0e3ebfc8006d.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/ply07b0e3ebfc8006d.tar.gz
 tar -xf /tmp/ply07b0e3ebfc8006d.tar.gz --overwrite -C /
-
+touch /data/test
   if [ -e /boot/volumio001 ]; then
   /usr/bin/sudo rm /boot/volumio001
   else
